d555b9324a4ca014026cb7d91ebd65112aee3ae5
Lab05_etec <Lab05_etec@etec>
2025-02-11 20:00:15 -0300

    Criação do arquivo index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>projeto PWII B</title>
+
+</head>
+
+<body>
+    <?php
+     echo "<h1> Titulo do projeto PWII B</h1>";
+    ?>
+</body>
+
+</html>
