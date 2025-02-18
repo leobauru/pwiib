80a6001316850001f591422c3c43e63e94893ba7
Leonardo Henrique Correa da Silva <leonardohenriquebauru@gmail.com>
2025-02-18 20:45:39 -0300

    Add files via upload

---
+++ pwiib/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>projeto PWII B</title>
+    <style>
+        .row{
+            border:2px solid red;
+            padding: 5px;
+        }
+        .rox > div {
+            border: 2px solid yellow;
+            padding: 50px, 0;
+            text-align:center;
+        }
+        </style>
+
+</head>
+
+<body>
+
+    <a href="contato.php">Página de contato</a>
+
+    <div class="row">
+        <div class="col-6">
+        <a href="produtos.php">Página de produtos</a>
+        <div class="col-6">
+  
+    </div>
+
+
+    <?php
+     echo "<h1> Titulo do projeto PWII B</h1>";
+    ?>
+    
+</body>
+
+</html>
---
+++ pwiib/produtos.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de produtos</title>
+
+</head>
+
+<body>
+
+    <a href="index.php">Página inicial</a>
+    <a href="contato.php">Entre em contato</a>
+    <h1>Lista de produtos</a>
+
+    <ol>
+    <?php
+       for($i = 0; $i < 3; $i++)
+       {
+         echo "<li>Item $i</li>";
+       }
+    ?>
+    
+</body>
+
+</html>
