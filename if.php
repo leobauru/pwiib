d932515f31c783d164b030a8e4e9102b77debb38
Lab05_etec <Lab05_etec@etec>
2025-02-28 21:42:09 -0300

    ?php

---
+++ if.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IF</title>
+</head>
+
+<body>
+
+<nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+</nav>
+
+<h1>if</h1>
+<h2>Estrutura de decisão</h2>
+A estrutura de decisão é composta por um teste booleano.
+Esse teste verifica uma condição se verdadeiro faça alguma ou se falso faça outra.
+
+<pre>
+    if(%numero == 1)
+    {
+        echo "Aqui signifca %numero igual a 1";
+    }else{
+        echo "Aqui significa %numero diferente de 1";
+    }
+</pre>
+
+<?php
+%numero = 2;
+
+ if(%numero == 1)
+ {
+     echo "Aqui signifca %numero igual a 1";
+ }else{
+     echo "Aqui significa %numero diferente de 1";
+ }
+ ?>
+</p>
+
+</body>
+</html>
