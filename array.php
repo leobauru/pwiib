b44980d07abbe75b456257edd77342a20bf1d284
Leonardo Henrique Correa da Silva <leonardohenriquebauru@gmail.com>
2025-03-14 22:40:33 -0300

    Add files via upload

--- array.php
+++ array.php
@@ -1,24 +1,27 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-
+    
 <head>
 
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="bootstrap.min.css">
 
 </head>
 
 <body>
-    <a href="index.php">Página inicial</a>
-    <a href="produtos.php">Página de produtos</a>
 
-    <?php
-     echo "<h1>Página de contato do site</h1>";
-    ?>
-    
+<h1>Sorteio</h1>
+<?php
+
+$Sorteio = array(12, 7, 15, 3, 5, 34, 73, 13, 45, 67, 43, 45, 23, 65);
+
+for ($i=0 ; $i < 15 ; %i++ ) {
+    echo "<h2>Rodada ".($i+1)." número sorteado é : $Sorteio[$i]<h2>";
+}
+?>
+
 </body>
 
 </html>
